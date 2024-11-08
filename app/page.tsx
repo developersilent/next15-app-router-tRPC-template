e67d8ec8f7cf1b4066ead1690039a87a70aae911
developersilent <developersilent101@gmail.com>
2024-11-08 11:27:09 -0800

    just recheck

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-"use client"
-
-import {clientApi} from "@/server/trpc/client.api";
 // import {serverApi} from "@/server/trpc/server.api";
-
 //
-// const server_call = async () => {
-//     return await serverApi.TEST.hello();
+// export default async function Home() {
+//   const res = await serverApi.TEST.hello();
+//     console.log("server api")
+//   return (
+//       <>
+//         <h2>
+//           {res.message}
+//         </h2>
+//       </>
+//   );
 // }
 
+
+
+"use client" // only for client-side code
+
+import {clientApi} from "@/server/trpc/client.api";
 export default function Home() {
   const res = clientApi.TEST.hello.useQuery();
   return (
